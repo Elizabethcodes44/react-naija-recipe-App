3304cc37cfceb011a601fa38e9bc57c63e5e6ed4
Elizabeth <lizbeepmc@gmail.com>
2024-09-08 15:39:20 +0300

    sign up styled

--- src/components/Home/signup.jsx
+++ src/components/Home/signup.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import background from "../../assets/land.png";
 const initialState = {
     name  : "",
     email : "",
@@ -8,6 +9,8 @@ const initialState = {
 }
 export default function Signup() {
     const [form , setForm] = useState(initialState);
+    const [message, setMessage] = useState(""); // For success or error messages
+  const [error, setError] = useState(""); // For error messages
     const handleChange = (e) => {
         setForm({
             ...form,
@@ -43,47 +46,83 @@ export default function Signup() {
     
             if (response.ok) {
                 console.log("Sign up successful", data);
+                setMessage("Login successful");
+        setError("");
                 navigate("/login");
             } else {
-                console.error("Sign up failed", data);
+                setMessage("");
+                setError(data.error || "Login failed");
             }
         } catch (error) {
             console.error("An error occurred", error);
+            setMessage("");
+      setError("An error occured during log in");
         }
     };
     
     
   return (
-    <div>
-      <form onSubmit = {handleSubmit}>
-      <label>
+      <div className="flex items-center justify-center min-h-screen text-white p-6"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover", // Ensures the image covers the entire area
+        backgroundRepeat: "no-repeat", // Prevents the image from repeating
+        backgroundPosition: "center", // Centers the background image
+        
+      }}>
+         <div className="bg-white/30 backdrop-blur-md p-8 rounded-lg shadow-lg max-w-sm w-full">
+        <h1 className="text-2xl font-bold mb-6 text-center">
+          Sign up on our blog
+        </h1>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        {message && <p className="text-green-500 mb-4">{message}</p>}
+      <form onSubmit = {handleSubmit} className="text-white space-y-4">
+      <div>
+      <label className="block text-gray-700 font-semibold mb-1 text-white">
           <span className="text-red-500">*</span>Name
         </label>
         <input placeholder="input your email" 
         value = {form.name}
         name = "name"
         type = "text"
-        onChange={handleChange}/>
-        <label>
+        onChange={handleChange}
+        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 bg-transparent text-white"
+              required/>
+               </div>
+               <div>
+        <label className="block text-gray-700 font-semibold text-white mb-1">
           <span className="text-red-500">*</span>EMail
         </label>
         <input placeholder="input your email" 
         value = {form.email}
         name = "email"
         type = "email"
-        onChange={handleChange}/>
-        <label><span className="text-red-500">*</span>Password</label>
+        onChange={handleChange}
+        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 bg-transparent text-white"
+              required/>
+              </div>
+              <div>
+        <label className="block text-gray-700 font-semibold text-white mb-1"><span className="text-red-500">*</span>Password</label>
         <input placeholder="input your password"
         value = {form.password}
         name = "password"
-        onChange={handleChange}/>
-        <label htmlFor="profileImage">Profile Image:</label>
-        <input type="file" name="profileImage" onChange={handleFileChange} />
-        <input type = "submit"
-        value="submit"
-        />
-      
+        onChange={handleChange}
+        className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 bg-transparent text-white"
+              required/>
+           </div> 
+           <div>
+        <label htmlFor="profileImage" className="block text-gray-700 font-semibold text-white mb-1">Profile Image:</label>
+        <input type="file" name="profileImage" onChange={handleFileChange} className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 bg-transparent text-white" />
+        </div>
+        <div className="flex items-center justify-between">
+        <button type = "submit"
+         className="bg-orange-600 font-semibold text-white px-4 py-2 rounded-lg shadow-md hover:bg-blue-700 transition duration-300"
+         >
+        Submit
+        </button>
+        </div>
       </form>
+      </div>
     </div>
   );
 }
